23845addfa1758e0dd60aaf9d1a65fd625d96bc4
Sgenne <simon.genne@gmail.com>
2022-02-04 19:39:35 +0100

    Added JSDoc to user.service and user.interface.

--- server/src/model/user.interface.ts
+++ server/src/model/user.interface.ts
@@ -1,17 +1,54 @@
 import { Thread } from "./thread.interface";
 
 export interface User {
+  /**
+   * The username of the user.
+   */
   username: string;
+
+  /**
+   * The email of the user.
+   */
   email: string;
+
+  /**
+   * The date at which the user joined the site.
+   */
   joinDate: Date;
+
+  /**
+   * The date of birth of the user.
+   */
   birthDate: any;
+
+  /**
+   * The biography of the user.
+   */
   bio?: string;
+
+  /**
+   * A hashed and salted version of the users password.
+   */
   passwordHash: string;
+
+  /**
+   * The profile picture of the user.
+   */
   image?: any;
+
+  /**
+   * The threads that the user has liked.
+   */
   likedThreads: any[]; //Threads type later
+
+  /**
+   * The threads that the user has disliked.
+   */
   unlikedThreads: any[];
 
-  // Determines which properties are visible on the profile page.
+  /**
+   * Indicates which of the users properties should be visible on their profile page.
+   */
   visibleProperties: {
     email: boolean;
     joinDate: boolean;
--- server/src/service/user.service.ts
+++ server/src/service/user.service.ts
@@ -1,7 +1,9 @@
 import { User } from "../model/user.interface";
 import bcrypt from "bcryptjs";
 
+// Temporary in-memory store.
 const users: { [key: string]: User } = {};
+
 interface PartialUser {
   username: string;
   birthDate?: any;
@@ -10,17 +12,25 @@ interface PartialUser {
   image?: any;
 }
 
+/**
+ * Updates the properties of a registered user.
+ *
+ * @param partialUser - An object whose birthDate, bio, password, and image
+ * properties will, if defined, be used to updated the user.
+ *
+ * @returns An object containing information about if the service succeeded
+ * and the updated user if one was updated.
+ */
 export const editUserInformation = async (
   partialUser: PartialUser
 ): Promise<{ message: string; statusCode: number; user?: User }> => {
-  // Todo: check that password is correct before editing user.
-
-  console.log("partialUser: ", partialUser);
+  /* 
+    Todo: check that password is correct before editing user.
+    Needs to take the password as a separate argument since we
+    want to be able to provide the password without updating it.
+  */
 
   const existingUser = users[partialUser.username];
-  
-  console.log("existingUser: ")
-
 
   if (!existingUser) {
     return { statusCode: 404, message: "User not found." };
@@ -52,6 +62,18 @@ export const editUserInformation = async (
   };
 };
 
+/**
+ * Attempts to sign a user in with the given username and password.
+ *
+ * @param username - The username to use when signing the user in.
+ *
+ * @param password - The password to use when signing the user in.
+ *
+ * @returns An object indicating the success of the attempted
+ * sign-in. If successfull, then the object will contain the user object
+ * of the signed in user.
+ *
+ */
 export const signIn = async (
   username: string,
   password: string
@@ -77,6 +99,20 @@ export const signIn = async (
   };
 };
 
+/**
+ * Attempts to register a user with the given information.
+ *
+ * @param email - The email of the new user.
+ *
+ * @param username - The username of the new user.
+ *
+ * @param password - The password of the new user.
+ *
+ * @param birthDate - The date of birth of the new user.
+ *
+ * @returns An object indicating success of the service.
+ * If the new user was registered, then the object will
+ */
 export const register = async (
   email: string,
   username: string,
@@ -125,6 +161,15 @@ export const register = async (
   };
 };
 
+/**
+ * Attempts to delete a user.
+ *
+ * @param username - The username of the user to delete.
+ *
+ * @param password - The password of the user to delete.
+ *
+ * @returns An object containing the result of the attempted service.
+ */
 export const deleteUser = async (
   username: string,
   password: string
@@ -141,21 +186,24 @@ export const deleteUser = async (
   );
 
   if (!passwordIsValid) {
-    return { statusCode: 401, message: "The given was invalid." };
+    return { statusCode: 401, message: "The given password was invalid." };
   }
 
   delete users[username];
 
-  return { statusCode: 200, message: "User was deleted successfully." };
+  return { statusCode: 200, message: "The user was deleted successfully." };
 };
 
+/**
+ * Returns the user object of the user with the given username, if one exists.
+ */
 export const getUser = async (
   username: string
 ): Promise<{ statusCode: number; message: string; user?: User }> => {
   const existingUser = users[username];
 
   if (!existingUser) {
-    return { statusCode: 404, message: "user not found." };
+    return { statusCode: 404, message: "User not found." };
   }
 
   const exposedUser = { ...existingUser, passwordHash: "" };
@@ -167,6 +215,17 @@ export const getUser = async (
   };
 };
 
+/**
+ * Updates the visible properties of the user with the given username and password.
+ *
+ * @param username - The username of the user to update.
+ *
+ * @param password - The password of the user to update.
+ *
+ * @param options  - An object describing which of the users properties should be visible.
+ *
+ * @returns An object containing the result of the attempted service.
+ */
 export const setVisibleProperties = async (
   username: string,
   password: string,
@@ -204,27 +263,53 @@ export const setVisibleProperties = async (
   };
 };
 
+/**
+ * Returns a boolean indicating if the given email address is valid.
+ */
 const validEmail = (email: string): boolean => {
   return true;
 };
+
+/**
+ * Returns a boolean indicating if the given image is valid.
+ */
 const validImage = (image: any): boolean => {
   return true;
 };
+
+/**
+ * Returns a boolean indicating if the given username is valid.
+ */
 const validusername = (username: string): boolean => {
   return true;
 };
 
+/**
+ * Returns a boolean indicating if the given password is valid.
+ */
 const validPassword = (password: string): boolean => {
   return true;
 };
 
+/**
+ * Returns a boolean indicating if the given date of birth is valid.
+ */
 const validBirthDate = (date: Date): boolean => {
   return true;
 };
 
+/**
+ * Returns a boolean indicating if the given bio is valid.
+ */
 const validBio = (bio: string): boolean => {
   return true;
 };
+
+/**
+ * Returns a hashed and salted version of the given password.
+ *
+ * @param password - The password to hash and salt.
+ */
 const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt();
   const hashed = await bcrypt.hash(password, salt);
